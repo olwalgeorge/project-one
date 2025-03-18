fc973e77fca1427c4571cd7fcfcb966b47fa386b
George Olwal <olwalgeorge@live.com>
2025-03-18 16:33:46 +0300

    added cors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ const routes = require("./routes");
 const connectDB = require("./config/database");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("../swagger_output.json");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+
+// Enable CORS for DEVELOPMENT (allow all origins)
+app.use(cors());
 
 // Serve Swagger UI at /api-docs
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
